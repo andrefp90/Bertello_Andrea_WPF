3babe80be63ffe50cdb315a337cf8405d8b49752
andrea <andrefp90@fullsail.edu>
2014-09-10 16:27:57 -0500

    calculated users age
    calculated users age

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -3,4 +3,30 @@ Andrea bertello
 sept 10 2014
 Go to training Week #2
  */
-alert("1,2,3!");
+//alert("Testing 1,2,3!");
+
+//Create a calculator for age
+
+//Ask the user their name
+var name=prompt("Please type in your name:");
+
+//test
+console.log(name);
+//Alert to the user and explain what we are doing
+alert("Welcome"+name+"! Let's go and figure out how old you are!");
+//Ask the user what year they where born
+//Create variable to catch the answer
+var yearBorn = prompt("What year where you born?");
+console.log(yearBorn);
+
+//Current year -year born
+
+var currentYear =2014;
+
+//Calculate the age
+var age= currentYear - yearBorn;
+//alert the user with their age
+
+alert(name+" you are "+age+" years old.");
+
+

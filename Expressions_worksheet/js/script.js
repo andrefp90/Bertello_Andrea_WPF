3081195fd6be6009011732bbc4464d6837f1a5e9
andrea <andrefp90@fullsail.edu>
2014-09-10 13:17:47 -0500

    Expression worksheet
    dog years problem 1

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,18 @@
+//Andrea Bertello Franco 11 september 2014 Expressions Worksheet
+
+//Dog years
+// Calculate sparky's age in dog years
+var dogAgeInHuman = 3;
+var dogAgeInDog = 7;
+
+//multiply dog age in human * dog age in dog
+
+var sparkyAge = dogAgeInHuman * dogAgeInDog;
+// var for console print out
+var result1 = ("Sparky is"+" "+dogAgeInHuman);
+var result2 =(" human years old which is"+ " "+sparkyAge);
+var result3 = ("in dog years");
+var final = ""+ String (result1)+" "+String(result2)+" "+String(result3);
+//print out “Sparky is X human years old which is X in dog years.
+console.log (final);
+

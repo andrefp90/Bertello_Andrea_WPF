179c10501dfffc3c7181581d96dfaf2cbb3cb025
andrea <andrefp90@fullsail.edu>
2014-09-11 17:07:08 -0500

    prompt user for height
    prompt user for height

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -7,7 +7,12 @@ Andrea Bertello Franco
 
 // Prompt for name and Introduction
 var name=prompt("Please type in your name:");
-console.log (name)
+console.log (name);
 alert("Welcome"+" "+name+"! Let's find out how many body's your height fit in a soccer field!");
+
+//Prompt for the user height
+var height=parseInt(prompt(name+" "+"What is your height?"));
+
+
 
 

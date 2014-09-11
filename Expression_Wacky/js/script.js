2a2929bf153488cc9c60298d64203c06ec175454
andrea <andrefp90@fullsail.edu>
2014-09-11 17:02:13 -0500

    Expression Wacky
    Expressions Wacky calculating your height how many body's your height
    would fit in a soccer field
    Prompt name and introduction

---
+++ Expression_Wacky/js/script.js
@@ -0,0 +1,13 @@
+/**
+Andrea Bertello Franco
+ 11 September 2014
+ Expressions Wacky
+ */
+//Expressions Wacky calculating your height how many body's your height would fit in a soccer field
+
+// Prompt for name and Introduction
+var name=prompt("Please type in your name:");
+console.log (name)
+alert("Welcome"+" "+name+"! Let's find out how many body's your height fit in a soccer field!");
+
+

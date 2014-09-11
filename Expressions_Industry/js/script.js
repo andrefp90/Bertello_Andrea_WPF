f4705679aa43b939035929bd77ca8312e976c537
andrea <andrefp90@fullsail.edu>
2014-09-11 14:16:53 -0500

    Expressions industry
    prompt name and introduction

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,14 @@
+/**
+Andrea Bertello Franco
+ 11 September 2014
+ Expressions Industry
+ */
+
+//Expressions Industry How much will you earn as a freelance in one project
+
+//Ask the user their name
+var name=prompt("Please type in your name:");
+//test
+console.log(name);
+//Alert to the user and explain what we are doing
+alert("Welcome"+name+"! Let's find out how much will you earn with a project!");

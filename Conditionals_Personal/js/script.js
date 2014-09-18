f745e739d93706ea53fa6416a0bd65418df467de
andrea <andrefp90@fullsail.edu>
2014-09-18 12:13:43 -0500

    if temperature is greater than 75
    if temperature is greater than 75

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -12,14 +12,20 @@ console.log (name)
 alert("Welcome"+" "+name+"! Let's see what you should wear!");
 
 //different temperatures to see is it's warm or cold
-var veyWarm =90;
+
 var warm = 75;
 var notTooCold = 60;
-var cold = 50;
+
 
 //prompt for temperature
 var temperature= prompt("What is the temperature");
 console.log ("The temperature is"+ " "+temperature+" "+"degrees celsius");
+
+//If the temperature is grater than 90 it's too hot
+if(temperature >warm ){
+    alert("It's too hot you should wear something light a dress or shorts")
+
+}
 
 
 

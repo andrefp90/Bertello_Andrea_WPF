9a1e8318eb44f5c831e76e48271e8e62a14a4252
andrea <andrefp90@fullsail.edu>
2014-09-18 16:56:01 -0500

    last chek ups
    last chek ups

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,7 +8,7 @@
 
 //name and welcome user explain how much will she/he work until he needs a break
 var name=prompt("Please type in your name:");
-console.log (name)
+console.log (name);
 alert("Welcome"+" "+name+" "+"!Let's check ot how many calories you have left in a 2000 calorie diet" +
     " starting with a 500 calorie breakfast");
 
@@ -17,7 +17,7 @@ var caloriesConsumed =parseInt(prompt(name+" "+"How many calories have you consu
 console.log("How many calories have you consumed"+" "+caloriesConsumed);
 
 //If user forgets to put input remind she/he
-if (hoursWorked === ""){
+if (caloriesConsumed=== ""){
     ("oops You forgot to write the answer: Please type in how many calories have you consumed");
 }
 // var for an obligatory breakfast calories
@@ -25,19 +25,22 @@ var breakfast = 500;
 //daily calories var
 var dailyCalories = 2000;
 
-//how many calories do you have left
-var caloriesLeft =dailyCalories-( breakfast += caloriesConsumed);
-console.log ("You have"+" "+caloriesLeft+" "+"Calories left");
-
 //how many calories did the user over passed
 var overPassed = ( breakfast + caloriesConsumed)% dailyCalories;
-console.log("How many calories did the user over passed by "+" "+overPassed);
+//how many calories do you have left
 
+var caloriesLeft =dailyCalories-( breakfast += caloriesConsumed);
+
+if(caloriesLeft < dailyCalories) {
+    console.log("You have" + " " + caloriesLeft + " " + "Calories left");
+}else if (caloriesLeft >= dailyCalories){
+    console.log("How many calories did the user over passed by " + " " + overPassed);
+}
 //if the calories and the breakfast is less than 2000
 
-if(breakfast && caloriesConsumed > dailyCalories){
+if(breakfast && caloriesConsumed < dailyCalories){
     console.log("You haven't reached the goal you have "+" "+caloriesLeft+" "+"calories left");
-    alert(name+" "+"You haven't reached the goal you have "+" "+caloriesLeft+" "+"calories left")
+    alert(name+" "+"You haven't reached the goal you have "+" "+caloriesLeft+" "+"calories left");
 //if the count is over 2000
 }else{
     console.log("You have over passed your goal!!");

57d26be9d315d3aee76708438115f618c66a6cf2
andrea <andrefp90@fullsail.edu>
2014-09-18 12:45:20 -0500

    conditional Industry
    welcome user and tell them if they need a break form work

---
+++ Conditional_Industry/js/script.js
@@ -0,0 +1,11 @@
+/**
+ Andrea Bertello Franco
+ 18 sept 2014
+ Conditional Industry
+ */
+//Time for a break
+
+    //name and welcome user explain how much will she/he work until he needs a break
+var name=prompt("Please type in your name:");
+console.log (name)
+alert("Welcome"+" "+name+"!Lets see after if you need a break from work!");

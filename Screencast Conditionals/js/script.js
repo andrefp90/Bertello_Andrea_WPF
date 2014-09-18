852f91057a34e3a07174ca17505b63b13cb41483
andrea <andrefp90@fullsail.edu>
2014-09-18 00:24:09 -0500

    ternary 2
    book example

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -5,16 +5,20 @@ Screencast conditionals
  */
 //conditional logic - Ternary Operators
 
-var gpa = 48;
+var age = 11;
+var book;
 
-//if the gpa is over the min 2.0 score, the student can graduate
-
-/*if(gpa>2.0){
-    console.log("You can graduate!");
+//iF THE CHILD IS UNDER 10, they get Green eggs and ham, otherwise they get the time machine
+/*if(age <10){
+    book = "Green eggs and ham";
 }else{
-    console.log ("GPA is too low!");
-}*/
-(gpa >2.0)? console.log("You can graduate!"):console.log ("GPA is too low!");
+    book = "The Time Machine";
+}
+console.log(book);
+*/
+book = (age<10)?"Green eggs and Ham ":"The time Machine";
+console.log(book);
+
 
 
 

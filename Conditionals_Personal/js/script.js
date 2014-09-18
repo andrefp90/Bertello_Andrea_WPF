3b583c928e4d634f0e65441aecb18fdcf29bd3e9
andrea <andrefp90@fullsail.edu>
2014-09-18 12:34:06 -0500

    if the temperature is less than 60
    wear something warm

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -21,11 +21,17 @@ var notTooCold = 60;
 var temperature= prompt("What is the temperature");
 console.log ("The temperature is"+ " "+temperature+" "+"degrees celsius");
 
-//If the temperature is grater than 90 it's too hot
-if(temperature >warm ){
-    alert("It's too hot you should wear something light a dress or shorts")
 
+
+//If the temperature is grater than 75 it's too hot
+if(temperature >warm ){
+    alert("It's too hot you should wear something light; a dress or shorts")
+//IF the temperature is less than 60
+}else if(temperature < notTooCold) {
+   alert ("It's cold wear something warm; use layers")
 }
+
+
 
 
 

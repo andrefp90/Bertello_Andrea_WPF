27d1544649b3421bee2a1b7de00084769234099b
andrea <andrefp90@fullsail.edu>
2014-09-17 23:19:06 -0500

    With an Expression
    With an Expression

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -7,11 +7,13 @@ Screencast conditionals
 
 var kidHeight = 40;
 var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight>minHeight){
+if(kidHeight +sneakerLift > minHeight){
     //code performed if condition is true
     console.log ("You can ride the coaster!");
 }
+
 

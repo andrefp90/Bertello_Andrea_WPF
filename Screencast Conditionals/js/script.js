a0637f98bd4595e87db7b28ea81a91f278d4141b
andrea <andrefp90@fullsail.edu>
2014-09-18 00:00:19 -0500

    logical operator &&
    logical operator &&

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -3,25 +3,21 @@ Andrea Bertello Franco
 Screencast conditionals
 17 sep 2104
  */
-//conditional logic
+//conditional logic -Logical Operators
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight =45; // the height of the height with the parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
 
-
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight  > minHeight){
-    //you can ride!
-    console.log ("You can ride the coaster!");
-}else if( kidHeight > wParentHeight){
-    //you can ride with a parent present
-  console.log("You can ride,but only with a parent present.")
-}else{
-    //sorry you have growing to do
-    ("Sorry kid, you've got some growing to do first!");
+//if the price of the phone is less our budget and if our paycheck is over 300
+if(iPhonePrice < budget && payCheck < 300){
+    console.log(" We can buy the phone!");
+ }else{
+    console.log("No phone for you!!");
 }
+
+
+
 
 
 

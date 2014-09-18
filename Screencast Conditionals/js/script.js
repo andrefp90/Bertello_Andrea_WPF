a194e15cee23897c931ecb34b9733ba21e85b65b
andrea <andrefp90@fullsail.edu>
2014-09-17 23:15:03 -0500

    Relational Expressions
    Relational Expressions

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -5,12 +5,13 @@ Screencast conditionals
  */
 //conditional logic
 
-var oldEnough = false;
+var kidHeight = 40;
+var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride!"
-
-if(oldEnough){
-    //code performed if condition is ture
+//if the kid is over 48 inches in height
+if(kidHeight>minHeight){
+    //code performed if condition is true
     console.log ("You can ride the coaster!");
 }
-console.log ("what comes after");
+

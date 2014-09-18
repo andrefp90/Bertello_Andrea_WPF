3599156e68bb559779f3a44a5b010f5b218969e1
andrea <andrefp90@fullsail.edu>
2014-09-18 16:22:39 -0500

    calories overpassed
    how many calories did the user pass

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,7 +9,8 @@
 //name and welcome user explain how much will she/he work until he needs a break
 var name=prompt("Please type in your name:");
 console.log (name)
-alert("Welcome"+" "+name+" "+"!Let's check ot how many calories you have left in a 2000 calorie diet");
+alert("Welcome"+" "+name+" "+"!Let's check ot how many calories you have left in a 2000 calorie diet" +
+    " starting with a 500 calorie breakfast");
 
 //ask the user how many calories have they consumed
 var caloriesConsumed =parseInt(prompt(name+" "+"How many calories have you consumed today?"));
@@ -24,15 +25,21 @@ var dailyCalories = 2000;
 var caloriesLeft =dailyCalories-( breakfast += caloriesConsumed);
 console.log ("You have"+" "+caloriesLeft+" "+"Calories left");
 
+//how many calories did the user over passed
+var overPassed = ( breakfast + caloriesConsumed)% dailyCalories;
+console.log("How many calories did the user over passed by "+" "+overPassed);
+
 //if the calories and the breakfast is less than 2000
 
 if(breakfast && caloriesConsumed > dailyCalories){
     console.log("You haven't reached the goal you have "+" "+caloriesLeft+" "+"calories left");
     alert(name+" "+"You haven't reached the goal you have "+" "+caloriesLeft+" "+"calories left")
+//if the count is over 2000
 }else{
     console.log("You have over passed your goal!!");
     alert(name+" "+"You have over passed your goal!!");
 }
+
 
 
 

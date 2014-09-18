f234d78632869cbd611ff9e58605bf7b3bdbc572
andrea <andrefp90@fullsail.edu>
2014-09-18 11:59:11 -0500

    prompt for temperature
    prompt for temperature

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,9 +11,16 @@ var name=prompt("Please type in your name:");
 console.log (name)
 alert("Welcome"+" "+name+"! Let's see what you should wear!");
 
+//different temperatures to see is it's warm or cold
 var veyWarm =90;
 var warm = 75;
 var notTooCold = 60;
 var cold = 50;
 
+//prompt for temperature
 var temperature= prompt("What is the temperature");
+console.log ("The temperature is"+ " "+temperature+" "+"degrees celsius");
+
+
+
+

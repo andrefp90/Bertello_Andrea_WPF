4f0d9dc5d9d921c1fe9bbdf36c21c54aaa2981f6
andrea <andrefp90@fullsail.edu>
2014-09-18 14:44:03 -0500

    calorie check
    welcome and inform user of how many calories she/he has left

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,13 @@
+/**
+ Andrea Bertello franco
+ 18 sept 2014
+ Conditionals Wacky
+ */
+
+//How many calories do you have left
+
+//name and welcome user explain how much will she/he work until he needs a break
+var name=prompt("Please type in your name:");
+console.log (name)
+alert("Welcome"+" "+name+" "+"!Let's check ot how many caloires you have left in a 2000 calorie diet");
+

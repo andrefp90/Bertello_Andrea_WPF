7e2cad53f25f732e04d209a86393964c12a9edad
andrea <andrefp90@fullsail.edu>
2014-09-17 23:12:31 -0500

    conditional logic
    conditional logic

---
+++ Screencast Conditionals/js/script.js
@@ -0,0 +1,16 @@
+/*
+Andrea Bertello Franco
+Screencast conditionals
+17 sep 2104
+ */
+//conditional logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+    //code performed if condition is ture
+    console.log ("You can ride the coaster!");
+}
+console.log ("what comes after");

b20581ad8a8a4091eae793440eacafc1e0a21743
andrea <andrefp90@fullsail.edu>
2014-09-22 12:45:58 -0500

    basic structure
    functions basic structure

---
+++ Functions/js/script.js
@@ -0,0 +1,17 @@
+/**
+ Andrea Bertello franco
+ 22 sep 2104
+ functions
+ */
+
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("hello world")
+}
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width *height;
+    console.log(area);
+}

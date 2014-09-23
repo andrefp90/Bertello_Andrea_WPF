5d532b08f7f5cb91866299db6ebe3231c9329f42
andrea <andrefp90@fullsail.edu>
2014-09-22 20:30:59 -0500

    function worksheet 1
    circumference

---
+++ Functions_Worksheets/js/script.js
@@ -0,0 +1,17 @@
+/**
+Andrea Bertello Franco
+ 22 sept 2014
+ Function Worksheet
+ */
+
+//Functions Worksheet
+
+//Circumference
+var total = calc(3.14,5)
+
+function calc (pi, diameter ){
+    var circumference = pi * diameter;
+    return circumference;
+
+}
+console.log (total);

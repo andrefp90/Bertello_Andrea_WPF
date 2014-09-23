0c5fc56c1261b559343092773e99bbabb7c1389d
andrea <andrefp90@fullsail.edu>
2014-09-22 20:48:34 -0500

    Function Worksheet Q2
    stung

--- Functions_Worksheets/js/script.js
+++ Functions_Worksheets/js/script.js
@@ -7,11 +7,22 @@ Andrea Bertello Franco
 //Functions Worksheet
 
 //Circumference
-var total = calc(3.14,5)
+var total = calc(3.14,5);
 
 function calc (pi, diameter ){
     var circumference = pi * diameter;
     return circumference;
 
 }
-console.log (total);
+console.log ("The circumference of a circle is"+ total);
+
+//Stung!
+
+var victimsW = beeStings(8.66,1000);
+
+function beeStings (bee , horse){
+    var kill = bee * horse;
+    return kill;
+}
+
+console.log ("It takes"+" "+victimsW+" "+"bee sting to kill a horse");

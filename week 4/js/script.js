3d87e0f3396d10271b25dc5e71d10774e2c61691
andrea <andrefp90@fullsail.edu>
2014-09-24 16:30:08 -0500

    got both user inputs
    got both user inputs

--- week 4/js/script.js
+++ week 4/js/script.js
@@ -18,5 +18,33 @@ while (min ===""){
     //Re-prompt the user
     min= prompt("Please don't leave blank.\n Type a number:");
 }
-
 console.log(min);
+
+//Test to make sure min is a number
+//isNaN()
+//- is what's inside Not a Number?'
+
+console.log(isNaN(7)); //false if it is a number
+console.log(isNaN("monkey"));//text gives us a true
+
+while(isNaN(min)){
+    //Reprompt user
+    min=prompt("Only type in number\nPlease type in a min number:");
+
+}
+
+
+//Ask the user for a max number
+
+var max= prompt("Please type in a max number:");
+//validate that prompt
+//best way so far to validate -combine the while loops
+
+while(max==="" || isNaN(max) ){
+    // && both have to be true to be true
+    // || one or both have to be true
+
+    //REPROMPT THE USER
+    max = prompt("Please don't leave blank or type in anything than numbers.\nPlease type a max number");
+
+}

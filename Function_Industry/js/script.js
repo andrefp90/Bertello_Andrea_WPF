f1a08166ba421e681af98d24321e91c4f2f7cfd9
andrea <andrefp90@fullsail.edu>
2014-09-24 22:21:07 -0500

    prompt user for the interest
    if the user forgets while loop

--- Function_Industry/js/script.js
+++ Function_Industry/js/script.js
@@ -21,7 +21,7 @@ alert("Welcome"+" "+name+"! Let's see how much you will have to pay for your loa
 
 //prompt for how much was the loan
 var loan =parseInt(prompt("How much did you ask for the loan?"));
-console.log(loan)
+console.log(loan);
 
 //if the user forgot to write input
 while (loan ===""|| isNaN(loan)){
@@ -30,4 +30,17 @@ while (loan ===""|| isNaN(loan)){
     loan= prompt("Please don't leave blank.\n Type in you name:");
     console.log(loan);
 }
+
+//prompt for the interest percentage
+var interest = parseInt(prompt("How much is the interest"));
+console.log(interest);
+//if the user forgot to write input
+while (interest ===""|| isNaN(interest)){
+
+    //Re-prompt the user
+    interest= prompt("Please don't leave blank.\n Type in the interest:");
+    console.log(interest);
+}
+
+
 

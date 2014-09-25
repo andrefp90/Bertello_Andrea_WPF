f1fcbcc33735087dd59fcf457e3543dea59f8fcb
andrea <andrefp90@fullsail.edu>
2014-09-25 16:11:33 -0500

    how much tv do you watch
    alert user of the calculator and prompt for name

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,19 @@
+/**
+ Andrea Bertello franco
+ 25/ sept/2014
+ Functions Wacky
+ */
+
+//How much tv do you watch in a month and a year
+
+var name=prompt("Please type in your name:");
+console.log (name);
+while (name ===""){
+
+    //Re-prompt the user
+    name= prompt("Please don't leave blank.\n Type in you name:");
+    console.log(name);
+}
+
+alert("Welcome"+" "+name+"! Let's see how much tv you watch in a year!");
+
